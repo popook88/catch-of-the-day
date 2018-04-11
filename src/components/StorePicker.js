e3c5eda289bde854639b8e589ec95ac71ec5c387
Kevin Kane <kkane@tableau.com>
2018-04-11 13:44:10 -0700

    finished routing

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-    render() {
-        return(
-        <React.Fragment>
-            <form className="store-selector">
-                <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Enter a store" />
-                <button type="submit">Visit Store</button>
-            </form>
-        </React.Fragment>
-        )
-    }
+  myInput = React.createRef();
+
+  goToStore = event => {
+    event.preventDefault();
+    const storeName = this.myInput.value.value;
+    this.props.history.push(`/store/${storeName}`);
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <form className="store-selector" onSubmit={this.goToStore}>
+          <h2>Please Enter A Store</h2>
+          <input
+            type="text"
+            ref={this.myInput}
+            required
+            placeholder="Enter a store"
+            defaultValue={getFunName()}
+          />
+          <button type="submit">Visit Store</button>
+        </form>
+      </React.Fragment>
+    );
+  }
 }
 
 export default StorePicker;
