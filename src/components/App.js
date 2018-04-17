188de7d33ef386521d12f90b300ee7b06c202857
Kevin Kane <kkane@tableau.com>
2018-04-17 16:29:36 -0700

    added state and passed it through to addFishForm

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,23 @@ import Inventory from "./Inventory";
 import Order from "./Order";
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    const fishes = { ...this.state.fishes };
+    fishes[`fish${Date.now()}`] = fish;
+    this.setState({ fishes });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header tagline="Fresh Seafood Market"/>
+          <Header tagline="Fresh Seafood Market" />
         </div>
-        {<Inventory />}
+        {<Inventory addFish={this.addFish} />}
         <Order />
       </div>
     );
