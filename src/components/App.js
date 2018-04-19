24b06f209c057a59c000b8eaf661a65f667627e0
Kevin Kane <kkane@tableau.com>
2018-04-19 11:09:02 -0700

    Load sample fishes works

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component {
   state = {
@@ -14,13 +15,22 @@ class App extends React.Component {
     fishes[`fish${Date.now()}`] = fish;
     this.setState({ fishes });
   };
+
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
         </div>
-        {<Inventory addFish={this.addFish} />}
+        {
+          <Inventory
+            addFish={this.addFish}
+            loadSampleFishes={this.loadSampleFishes}
+          />
+        }
         <Order />
       </div>
     );
